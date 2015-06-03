bd9e29e7040588acddbf40bbc7af2d7e454ac46e
Mafooch <jpmaff@yahoo.com>
2015-06-03 16:49:52 -0500

    add recently_finished and finished scopes to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,13 @@ class Book < ActiveRecord::Base
   def finished?
     finished_on.present?
   end
+
+  scope :finished, ->{ where.not('finished_on: nil') }
+  # '-> is for adding a proc'
+  scope :recently_finished, ->{ where('finished_on > ?', 10.days.ago) }
+
+  # could write as class method below
+  # def self.recently_finished
+  #   where('finished_on > ?', 10.days.ago)
+  # end
 end
